8993ed4d1a702f1e5eed7c6280f4a0db213e98a3
kmartindev <i@kmartin.ru>
2022-11-05 13:04:53 +0300

    test about context

---
+++ main.js
@@ -0,0 +1,24 @@
+const guitarStore = function () {
+  this.guitarCount = 2;
+
+  (function() {
+    this.guitarCount = 1;
+  })();
+
+  console.log(this.guitarCount);
+};
+
+const guitarShop = {
+  guitarCount: 3,
+  showMeAnswer: function() {
+    console.log(this.guitarCount);
+  },
+};
+
+// guitarStore();
+// new guitarStore();
+// guitarShop.showMeAnswer();
+// new guitarShop.showMeAnswer();
+// guitarStore.apply(guitarStore);
+// guitarShop.showMeAnswer.apply(guitarStore);
+// guitarStore.bind({})();
